70e2b481f20b26c63d1eaf01dfa819c37e672e18
ShaadAbdullah <shaad9414@gmail.com>
2025-07-22 14:21:32 +0530

    Added new Product Category model

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -424,7 +424,6 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
